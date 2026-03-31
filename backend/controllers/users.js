9c5b6a0be87807ede32bb4e42f0628afd9747880
BiancaBaccin <bianca.baccin@gmail.com>
2026-03-31 19:16:24 -0300

    Cria controller Login em Users

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,39 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email })
+    .select("+password")
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ message: "Email ou senha incorretos" });
+      }
+
+      return bcrypt.compare(password, user.password).then((matched) => {
+        if (!matched) {
+          return res.status(401).json({ message: "Email ou senha incorretos" });
+        }
+
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+          expiresIn: "7d",
+        });
+
+        return res.send({ token });
+      });
+    })
+    .catch(() => {
+      res.status(401).send({ message: "Email ou senha incorretos" });
+    });
+};
 
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.json(users))
     .catch(() =>
-      res.status(500).json({ message: "Ocorreu um erro no servidor" })
+      res.status(500).json({ message: "Ocorreu um erro no servidor" }),
     );
 };
 
@@ -43,7 +72,7 @@ module.exports.updateUserProfile = (req, res) => {
   User.findByIdAndUpdate(
     userId,
     { name, about },
-    { new: true, runValidators: true }
+    { new: true, runValidators: true },
   )
     .orFail()
     .then((user) => res.json(user))
