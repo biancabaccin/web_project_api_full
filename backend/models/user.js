1e321fb640aece4e0d215a207cc45cc2946d9e41
BiancaBaccin <bianca.baccin@gmail.com>
2026-03-31 18:52:30 -0300

    Atualiza userSchema, adiciona Email e Password e tira required de Name About e Avatar

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,28 +1,44 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: "Jacques Cousteau",
   },
   about: {
     type: String,
-    required: true,
     minlength: 2,
     maxlength: 30,
+    default: "Explorer",
   },
   avatar: {
     type: String,
-    required: true,
+    default:
+      "https://practicum-content.s3.us-west-1.amazonaws.com/resources/moved_avatar_1604080799.jpg",
     validate: {
       validator: function (v) {
         return /^https?:\/\/(www\.)?[a-zA-Z0-9._~:/?%#\[\]@!$&'()*+,;=-]+#?$/.test(
-          v
+          v,
         );
       },
       message: "URL do avatar inválida",
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: "Email inválido",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
     },
   },
 });
